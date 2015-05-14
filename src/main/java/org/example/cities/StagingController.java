45c32832850a9aaf8fbf665d36afd62346c0f6eb
Chris Busch <cbusch@pivotal.io>
2015-05-14 09:49:36 -0500

    update the date string

--- src/main/java/org/example/cities/StagingController.java
+++ src/main/java/org/example/cities/StagingController.java
@@ -1,5 +1,7 @@
 package org.example.cities;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,10 @@ public class StagingController {
     @RequestMapping("/")
     public String index() {
         String timestamp = System.getProperty("staging.timestamp");
-        if (timestamp == NULL) timestamp = new Date().toString();
+        
+        if (timestamp == null) 
+        	timestamp = new Date().toString();
+        
         return "Application was staged at: " + timestamp;
     }
 }
